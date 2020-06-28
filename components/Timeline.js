b7377b55b172e18bd8531f7a86131448c408f152
Nicholas Marino <nsmarino0@gmail.com>
2020-06-28 12:20:00 -0400

    more dummy data and timeline marker fix

--- components/Timeline.js
+++ components/Timeline.js
@@ -3,9 +3,12 @@ import { useState, useRef, useEffect } from 'react'
 
 import styles from './Timeline.module.css'
 
-// Would love to get rid of the useRefs, maybe replace with effect hooks or state stored in a higher component
+// RUNS TERRIBLY AND IS COMPLICATED AND WORDY -- BUT IT WORKS.
+// - find source of sluggishness
+// - effect/state hooks instead of ref hooks?
 
 export default function Timeline({essayData, navData}) {
+
   // sets timeline CSS to 'sticky' on scroll
   const [sticky, setSticky] = useState(false)
   const scrollRef = useRef()
@@ -17,7 +20,10 @@ export default function Timeline({essayData, navData}) {
   // governs preview image and location
   const [preview, setPreview] = useState(null)
   const [previewLocation, setPreviewLocation] = useState('')
+  const [isEdge, setIsEdge] = useState(false)
+  const [edgeMarkerLocation, setEdgeMarkerLocation] = useState('')
 
+  // Scroll handling
   // this has a hardcoded value of '32' still -- should be changed before deployment
   const handleScroll = () => {
     if (scrollRef.current) {
@@ -29,7 +35,6 @@ export default function Timeline({essayData, navData}) {
       }
     }
   }
-
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
@@ -38,7 +43,8 @@ export default function Timeline({essayData, navData}) {
     };
   }, []);
 
-  useEffect(() => {
+  // Width handling
+  const getBlockLocations = () => {
     const blockLocations = []
     blocksRef.current.childNodes.forEach(block => {
       const blockLocation = {
@@ -48,30 +54,39 @@ export default function Timeline({essayData, navData}) {
       blockLocations.push(blockLocation)
     })
     setBlockSpans(blockLocations)
+  }
+  useEffect(() => {
+    getBlockLocations()
+    window.addEventListener('resize', getBlockLocations)
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', getBlockLocations);
+    }
   }, [])
 
-
   const findMatchingBlock = (x) => {
-    const matchingSpan = blockSpans.find(block => block.left < x && x < block.right)
+    // supposedly a simple FOR LOOP is way more performant than array.find() ???
+    const matchingSpan = blockSpans.find(block => block.left <= x && x < block.right)
     const spanIndex = blockSpans.indexOf(matchingSpan)
     return navData[spanIndex]
   }
 
   const updateMouseX = e => {
     const rightEdge = Math.min(document.documentElement.clientWidth, window.innerWidth) // account for scrollbar
-
     const mouseLocation = e.clientX
-
     const matchingBlock = findMatchingBlock(mouseLocation)
     setPreview(matchingBlock)
 
     const adjustLocation = (location) => {
-      if (rightEdge - location < 150) return rightEdge - 300
-      else if (location < 150) return 0
-      else return location - 150
+      if (rightEdge - location < 150) return {edge: true, adjust: rightEdge - 300, marker: location - rightEdge + 300}
+      else if (location < 150) return {edge: true, adjust: 0, marker: location}
+      else return {edge: false, adjust: location - 150, marker: null}
     }
     const adjustedLocation = adjustLocation(mouseLocation)
-    setPreviewLocation(adjustedLocation)
+
+    setPreviewLocation(adjustedLocation.adjust)
+    setIsEdge(adjustedLocation.edge)
+    setEdgeMarkerLocation(adjustedLocation.marker)
   }
 
   const showTimeline = () => {
@@ -91,15 +106,10 @@ export default function Timeline({essayData, navData}) {
   return (
     <div ref={scrollRef} className={sticky ? styles.sticky : ''} onMouseLeave={() => setPreview(null)} >
       <div className={styles.navContainer}>
-
         <div className={styles.nav} ref={blocksRef}>
           {showTimeline()}
         </div>
-
       </div>
-
-
-
 
       { preview ?
       <div className={styles.thumbnailContainer} 
@@ -107,23 +117,30 @@ export default function Timeline({essayData, navData}) {
            onMouseMove={() => updateMouseX(event)}
            >
 
-           <div className={styles.markerFlexbox}>
-             <div className={styles.marker}></div>
-           </div>
+        {isEdge ?
+          <div style={{
+            width:'3px',
+            height: '2rem',
+            backgroundColor:'#333',
+            position: 'absolute',
+            top: '-2rem',
+            left: `${edgeMarkerLocation}px`,
+          }}></div>
+          :
+        <div className={styles.markerFlexbox}>
+          <div className={styles.marker}></div>
+        </div>
+        } 
 
-           <Link href='/essays/[dir]/[id]' as={`/essays/${essayData.dir}/${preview.id}`}>
-             <a>
-               <img src={preview.image} alt={preview.alt} className={styles.thumbnail} />
-             </a>
-           </Link>
-
+        <Link href='/essays/[dir]/[id]' as={`/essays/${essayData.dir}/${preview.id}`}>
+          <a>
+            <img src={preview.image} alt={preview.alt} className={styles.thumbnail} />
+          </a>
+        </Link>
       </div>
           :
         null
       }
-      <style jsx>{`
-
-      `}</style>
     </div>
   )
 }
